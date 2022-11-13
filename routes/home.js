e33ececec5c45892a0a1d408b2ff48a196385e60
Claire Bourdon <cebourdon625@gmail.com>
2022-11-13 18:30:49 -0500

    finished app

--- routes/home.js
+++ routes/home.js
@@ -5,8 +5,8 @@ const express = require('express')
 const router = express.Router()
 const homeController = require('../controllers/home')
 
-router.get('/', homeController.getIndex)
-router.post('/new', homeController.createTicket)
+router.get('/', homeController.getIndex) //read
+router.post('/new', homeController.createTicket) //create
 
 
 module.exports = router
